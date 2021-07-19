30495c143fc2362936b8fa09c2f904a0e887f541
Leonid Seniukov <mr.senukov@mail.ru>
2021-07-19 15:57:08 +0300

    add more controller tests

--- src/test/java/com/example/springbootrest/controller/ControllerDeleteTests.java
+++ src/test/java/com/example/springbootrest/controller/ControllerDeleteTests.java
@@ -1,15 +1,23 @@
 package com.example.springbootrest.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.application.SpringBootRestApplication;
+import org.application.exceptions.UserException;
+import org.application.model.mapping.UserMapper;
 import org.application.model.user.UserData;
+import org.application.model.user.UserDataDTO;
+import org.application.service.UserServiceMessageHelper;
 import org.application.service.UserStorageService;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,8 +25,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
@@ -31,12 +38,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ControllerDeleteTests {
 
     @Autowired
+    private UserServiceMessageHelper userServiceMessageHelper;
+
+    @Autowired
     private UserStorageService userStorageService;
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserMapper userMapper;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     private static final PathControllerHelper pathControllerHelper = new PathControllerHelper();
+
+    private boolean listOfUsersContainsEmail(String emailToFind) {
+        return userStorageService.getAllUsers().stream()
+                .map(UserData::getEmail)
+                .anyMatch(email -> email.equals(emailToFind));
+    }
+
+    private long getIdNotPresent() {
+        return -1001;
+    }
 
     private UserData getPreSavedUser() {
         return preSavedUser;
@@ -52,7 +77,7 @@ class ControllerDeleteTests {
     @Test
     void userNotGetsDeletedUserExceptionNotThrown() throws Exception {
 
-        long idNotPresent = -1000;
+        long idNotPresent = getIdNotPresent();
         userStorageService.saveUser(new UserData(null, "Test1", "test1@gmail.com"));
 
 
@@ -74,6 +99,7 @@ class ControllerDeleteTests {
 
         mockMvc.perform(get(pathControllerHelper.getRootPath()))
                 .andDo(print());
+
         mockMvc.perform(delete(pathControllerHelper.getRootPath() + preSavedUser.getId()))
                 .andDo(print())
                 .andExpect(MockMvcResultMatchers.status().isNoContent());
@@ -81,25 +107,68 @@ class ControllerDeleteTests {
         mockMvc.perform(get(pathControllerHelper.getRootPath()))
                 .andExpect(content().string(allOf(Matchers.not(containsString(
                         getPreSavedUser().getEmail())))));
-    }
-    @Test
-    void userGetsDeleted2() throws Exception {
-        userStorageService.saveUser(new UserData(null, "Test3", "test3@gmail.com"));
 
-        mockMvc.perform(get(pathControllerHelper.getRootPath()))
-                .andExpect(content().string(containsString(
-                        getPreSavedUser().getEmail())));
+        Assertions.assertFalse(listOfUsersContainsEmail(getPreSavedUser().getEmail()));
+    }
+
+    @Test
+    void userGetsUpdated() throws Exception {
+        UserData userDataUpdated = new UserData(null, "UpdatedUserName", "updated_email@gmail.com");
+        UserDataDTO userDataUpdatedDTO = userMapper.toUserDataDTO(userDataUpdated);
 
         mockMvc.perform(get(pathControllerHelper.getRootPath()))
                 .andDo(print());
 
-        mockMvc.perform(delete(pathControllerHelper.getRootPath() + preSavedUser.getId()))
+        String userAsString = objectMapper.writeValueAsString(userDataUpdatedDTO);
+
+        mockMvc.perform(put(pathControllerHelper.getRootPath() + preSavedUser.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userAsString))
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.status().isNoContent());
+                .andExpect(MockMvcResultMatchers.status().isOk());
 
         mockMvc.perform(get(pathControllerHelper.getRootPath()))
-                .andExpect(content().string(allOf(Matchers.not(containsString(
-                        getPreSavedUser().getEmail())))));
+                .andExpect(content().string(containsString(
+                        userDataUpdated.getEmail())));
+
+        Assertions.assertTrue(listOfUsersContainsEmail(userDataUpdated.getEmail()));
     }
 
+    @Test
+    void whenUpdatedUserNotFoundExceptionThrown() throws Exception {
+        UserData userDataUpdated = new UserData(null, "UpdatedUserName", "updated_email@gmail.com");
+        UserDataDTO userDataUpdatedDTO = userMapper.toUserDataDTO(userDataUpdated);
+
+        mockMvc.perform(get(pathControllerHelper.getRootPath()))
+                .andDo(print());
+
+        String userAsString = objectMapper.writeValueAsString(userDataUpdatedDTO);
+
+        mockMvc.perform(put(pathControllerHelper.getRootPath() + getIdNotPresent())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userAsString))
+                        .andDo(print())
+                        .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        Assertions.assertFalse(listOfUsersContainsEmail(userDataUpdated.getEmail()));
+    }
+
+    @Test
+    void userGetSaved() throws Exception {
+        UserData userDataUpdated = new UserData(null, "SavedUserName", "saved_email@gmail.com");
+        UserDataDTO userDataUpdatedDTO = userMapper.toUserDataDTO(userDataUpdated);
+
+        mockMvc.perform(get(pathControllerHelper.getRootPath()))
+                .andDo(print());
+
+        String userAsString = objectMapper.writeValueAsString(userDataUpdatedDTO);
+
+        mockMvc.perform(post(pathControllerHelper.getRootPath())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userAsString))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+        Assertions.assertTrue(listOfUsersContainsEmail(userDataUpdated.getEmail()));
+    }
 }
